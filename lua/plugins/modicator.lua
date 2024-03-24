bf5a27f19de5e31b04ba3c7e7434fd38f6803a0a
kyufie <imkyufie@gmail.com>
2024-03-24 20:38:39 +0700

    Shift to everforest-nvim and some other changes

--- lua/plugins/modicator.lua
+++ lua/plugins/modicator.lua
@@ -1,9 +1,8 @@
 local M = { 'mawkler/modicator.nvim' }
 
-M.dependencies = 'sainnhe/everforest'
-
 M.init = function()
     vim.o.cursorline = true
+    vim.o.cursorlineopt = 'number'
     vim.o.number = true
     vim.o.termguicolors = true
 end
