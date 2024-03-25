7236ea7fb0055a67c3ec31358293b65927d4c252
kyufie <imkyufie@gmail.com>
2024-03-25 18:40:39 +0700

    Disable italics

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -12,7 +12,7 @@ M.config = function()
     ---components be transparent (e.g. status line background)
     transparent_background_level = 2,
     ---Whether italics should be used for keywords and more.
-    italics = true,
+    italics = false,
     ---Disable italic fonts for comments. Comments are in italics by default, set
     ---this to `true` to make them _not_ italic!
     disable_italic_comments = true,
