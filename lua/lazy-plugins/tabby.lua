4c9f3c0c15fe4770a1530cbbe97f10f69a12315c
kyufie <imkyufie@gmail.com>
2024-12-08 08:38:19 +0700

    Remove redundant components on the tabline

--- lua/lazy-plugins/tabby.lua
+++ lua/lazy-plugins/tabby.lua
@@ -4,12 +4,13 @@ local M = { 'nanozuki/tabby.nvim' }
 M.dependencies = 'nvim-tree/nvim-web-devicons'
 
 M.config = function()
-    -- Always show tabline
-    vim.o.showtabline = 2
+  -- Always show tabline
+  vim.o.showtabline = 2
 
-    local theme = {
+  local theme = {
     fill = 'TabLineFill',
-    -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+    -- Also you can do this:
+    -- fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
     head = 'TabLine',
     current_tab = 'TabLineSel',
     tab = 'TabLine',
@@ -28,29 +29,12 @@ M.config = function()
         return {
           line.sep('', hl, theme.fill),
           tab.is_current() and '' or '󰆣',
-          tab.number(),
           tab.name(),
-          tab.close_btn(''),
           line.sep('', hl, theme.fill),
           hl = hl,
           margin = ' ',
         }
       end),
-      line.spacer(),
-      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-        return {
-          line.sep('', theme.win, theme.fill),
-          win.is_current() and '' or '',
-          win.buf_name(),
-          line.sep('', theme.win, theme.fill),
-          hl = theme.win,
-          margin = ' ',
-        }
-      end),
-      {
-        line.sep('', theme.tail, theme.fill),
-         { ' 󰄛 ', hl = theme.tail },
-      },
       hl = theme.fill,
     }
   end)
