8bafb7fc3b0f7136ad453d73028df8ac03410726
kyufie <imkyufie@gmail.com>
2025-04-26 15:26:14 +0700

    Refactor LSP plugins

--- lua/lazy-plugins/lspsaga.lua
+++ lua/lazy-plugins/lspsaga.lua
@@ -23,6 +23,55 @@ M.opts = {
   }
 }
 
+M.keys = {
+  {
+    'gr',
+    '<cmd>Lspsaga finder ref<CR>',
+    desc = '[G]oto [R]eference'
+  },
+  {
+    -- Overrides default keymap
+    'gi',
+    '<cmd>Lspsaga finder imp<CR>',
+    desc = '[G]oto [I]mplementation'
+  },
+  {
+    'gd',
+    '<cmd>Lspsaga finder def<CR>',
+    desc = '[G]oto [D]efinition'
+  },
+  {
+    'gD',
+    '<cmd>Lspsaga diagnostic_jump_next<CR>',
+    desc = '[G]oto next [D]iagnostic'
+  },
+  {
+    '<leader>la',
+    '<cmd>Lspsaga code_action<CR>',
+    desc = 'Code [A]ction'
+  },
+  {
+    '<leader>lr',
+    '<cmd>Lspsaga rename<CR>',
+    desc = '[R]ename'
+  },
+  {
+    '<leader>lci',
+    '<cmd>Lspsaga incoming_calls<CR>',
+    desc = '[I]ncoming [C]alls'
+  },
+  {
+    '<leader>lco',
+    '<cmd>Lspsaga outgoing_calls<CR>',
+    desc = '[O]utgoing [C]alls'
+  },
+  {
+    'K',
+    '<cmd>Lspsaga hover_doc<CR>',
+    desc = 'Hover Documentation'
+  },
+}
+
 M.dependencies = {
   'nvim-treesitter/nvim-treesitter',
   'nvim-tree/nvim-web-devicons',
--- lua/lazy-plugins/nvim-lspconfig.lua
+++ lua/lazy-plugins/nvim-lspconfig.lua
@@ -3,7 +3,12 @@ local M = { 'neovim/nvim-lspconfig' }
 local servers = {
   clangd = {
     cmd = {
-      "clangd --background-index --clang-tidy --completion-style=detailed --function-arg-placeholders --pch-storage=memory"
+      'clangd',
+      '--background-index',
+      '--clang-tidy',
+      '--completion-style=detailed',
+      '--function-arg-placeholders',
+      '--pch-storage=memory'
     }
   },
 
@@ -34,133 +39,36 @@ M.dependencies = {
   { 'j-hui/fidget.nvim', opts = {} },
   { 'folke/lazydev.nvim', opts = {} },
   'hrsh7th/nvim-cmp',
-  'nvim-telescope/telescope.nvim',
-  'nvimdev/lspsaga.nvim'
 }
 
 M.config = function()
-  -- [[ Configure LSP ]]
-  --  This function gets run when an LSP connects to a particular buffer.
-  local on_attach = function(_, bufnr)
-    local t_builtin = require('telescope.builtin')
-    local keybinds = {
-      {
-        key = 'gr',
-        func = '<cmd>Lspsaga finder ref<CR>',
-        desc = '[G]oto [R]eference'
-      },
-      {
-        -- Overrides default keymap
-        key = 'gi',
-        func = '<cmd>Lspsaga finder imp<CR>',
-        desc = '[G]oto [I]mplementation'
-      },
-      {
-        key = 'gd',
-        func = '<cmd>Lspsaga finder def<CR>',
-        desc = '[G]oto [D]efinition'
-      },
-      {
-        key = 'gD',
-        func = '<cmd>Lspsaga diagnostic_jump_next<CR>',
-        desc = '[G]oto next [D]iagnostic'
-      },
-
-      {
-        key = '<leader>la',
-        func = '<cmd>Lspsaga code_action<CR>',
-        desc = 'Code [A]ction'
-      },
-      {
-        key = '<leader>lr',
-        func = '<cmd>Lspsaga rename<CR>',
-        desc = '[R]ename'
-      },
-      {
-        key = '<leader>ls',
-        func = t_builtin.lsp_document_symbols,
-        desc = '[S]ymbols (local)'
-      },
-      {
-        key = '<leader>lS',
-        func = t_builtin.lsp_dynamic_workspace_symbols,
-        desc = '[S]ymbols (workspace)'
-      },
-      {
-        key = '<leader>lci',
-        func = '<cmd>Lspsaga incoming_calls<CR>',
-        desc = '[I]ncoming [C]alls'
-      },
-      {
-        key = '<leader>lco',
-        func = '<cmd>Lspsaga outgoing_calls<CR>',
-        desc = '[O]utgoing [C]alls'
-      },
-      {
-        key = '<leader>ld',
-        func = function(t) t_builtin.diagnostics(t, 0) end,
-        desc = '[D]iagnostics'
-      },
-      -- See `:help K` for why this keymap
-      {
-        key = 'K',
-        func = '<cmd>Lspsaga hover_doc<CR>',
-        desc = 'Hover Documentation'
-      },
-      {
-        key = '<C-k>',
-        func = vim.lsp.buf.signature_help,
-        desc = 'Signature Documentation'
-      },
-      {
-        key = '<leader>wa',
-        func = vim.lsp.buf.add_workspace_folder,
-        desc = '[W]orkspace [A]dd Folder'
-      },
-      {
-        key = '<leader>wr',
-        func = vim.lsp.buf.remove_workspace_folder,
-        desc = '[W]orkspace [R]emove Folder'
-      },
-      {
-        key = '<leader>wl',
-        func = function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end,
-        desc = '[W]orkspace [L]ist Folders'
-      }
-    }
-
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = 'LSP: ' .. desc
-      end
-
-      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    for _, kb in ipairs(keybinds) do
-      nmap(kb.key, kb.func, kb.desc)
-    end
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-      vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
-  end
-
   -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+  vim.lsp.config('*', {
+    capabilities = capabilities
+  })
+
   for server_name, opts in pairs(servers) do
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = opts,
-      filetypes = (opts or {}).filetypes,
-    }
+    vim.lsp.enable(server_name)
+    vim.lsp.config(server_name, opts)
   end
 end
+
+M.lazy = false
+
+M.keys = {
+  {
+    '<leader>wa',
+    vim.lsp.buf.add_workspace_folder,
+    desc = '[W]orkspace [A]dd Folder'
+  },
+  {
+    '<leader>wr',
+    vim.lsp.buf.remove_workspace_folder,
+    desc = '[W]orkspace [R]emove Folder'
+  },
+}
 
 return M
